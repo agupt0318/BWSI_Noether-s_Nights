c861b7c00883b0128ff5851366623cd36f178d9f
Nengyi-Jonathan-Jiang <theNerdyMango@gmail.com>
2024-07-24 15:23:10 -0500

    Refactoring Optimization.py
    Co-authored-by: DaniaHussein <dhussein@uiowa.edu>

--- src/classical/Optimization.py
+++ src/classical/Optimization.py
@@ -1,4 +1,4 @@
-# psuedocode basis is from Appendix C of the paper A Variational Quantum Attack for AES-like
+# Algorithms taken from Appendix C of the paper A Variational Quantum Attack for AES-like
 # Symmetric Cryptography (Wang et al., 2022)
 from typing import Callable
 
@@ -29,7 +29,8 @@ def gradient_descent(
         guess: np.ndarray,
         cost_function: Callable[[np.ndarray], float],
         learning_rate: float,
-        cost_cutoff: float
+        cost_cutoff: float,
+        num_iterations: int = 1024
 ) -> np.ndarray:
     f = 0
     num_dimensions = len(guess)
@@ -37,7 +38,7 @@ def gradient_descent(
     # noinspection PyTypeChecker
     best_guess: tuple[np.ndarray, float] = (None, float('inf'))
 
-    for ii in range(1024):
+    for _ in range(num_iterations):
         f += num_dimensions + 1
 
         cost_at_current_guess = cost_function(guess)
@@ -45,9 +46,8 @@ def gradient_descent(
         # If the cost is less than the cutoff, stop
         if cost_at_current_guess < cost_cutoff:
             return guess
-
+        # Otherwise, update the current best guess
         elif cost_at_current_guess < best_guess[1]:
-            # Update the current best guess
             best_guess = (guess.copy(), cost_at_current_guess)
 
         gradient = calculate_gradient_at_point(guess, cost_at_current_guess, cost_function, num_dimensions)
@@ -66,53 +66,53 @@ def gradient_descent(
     return best_guess[0]
 
 
-def n_m_method(cost_function, guess, alpha, xerr):
+def n_m_method(cost_function, guess, alpha, cost_cutoff):
     """
     generates the N other points (x1...xN)
 
     :param cost_function: implemented cost_function for computations
     :param guess: initial point
     :param alpha: amplification factor
-    :param xerr: cut-off condition
+    :param cost_cutoff: cut-off condition
     :return: new value of guess for iteration above in points
     """
 
     N = len(guess)  # N dimensions of guess
     points = [guess]  # list of x1--xN values
 
-    # calculating xi component on guess
-    def calc_xi_component():
+    # calculating xi component
+    def calc_xi_component(step: int):  # step should be 0 or 1
+        xi = guess.copy()
         for i in range(N):
-            xi = guess.copy()
             if guess[i] == 0:
                 xi[i] = 0.8
             else:
                 xi[i] = guess[i] * alpha
-        return xi
+            points[i + step] = xi
 
-    points.append(calc_xi_component())
+    calc_xi_component(0)
 
     times = N + 1
     while times < 1024:
         points.sort(key=lambda x: cost_function(x))
-        if cost_function(points[0]) <= xerr:
+        if cost_function(points[0]) <= cost_cutoff:
             break
         if cost_function(points[-1]) - cost_function(points[1]) < 0.15:
-            points[i + 1] = calc_xi_component()
+            calc_xi_component(1)
             continue
 
-        # calculate average of first N points
+        # calculate average of first N points m
         m = np.mean(points[:-1], axis=0)
-        # calculate reflect point
+        # calculate reflect point r
         r = 2 * m - points[-1]
         times += 1
 
-        if (cost_function(points[0]) <= cost_function(r) < cost_function(points[-2])):
+        if cost_function(points[0]) <= cost_function(r) < cost_function(points[-2]):
             points[-1] = r
             continue
 
         if cost_function(r) < cost_function(points[0]):
-            # calculate expand point
+            # calculate expand point s
             s = m + 2 * (m - points[-1])
             times += 1
             if cost_function(s) < cost_function(r):
