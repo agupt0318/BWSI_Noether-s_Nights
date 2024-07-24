86744d14cc21468b0ca74be13928fa0a8c6b6607
Emily Oberleitner <emilyober17@gmail.com>
2024-07-24 14:54:48 -0500

    turning plaintext into circuit Co-authored-by: Anant Gupta <agupt0318@users.noreply.github.com>

---
+++ src/quantum/plaintext_encoding.py
@@ -0,0 +1,10 @@
+from qiskit import QuantumCircuit,QuantumRegister
+
+def plaintext_circuit(plaintext):
+    register = QuantumRegister(len(plaintext))
+    circuit = QuantumCircuit(register)
+    for i in range(len(plaintext)):
+        if int(plaintext[i]) == 1:
+            circuit.x(i)
+    return circuit
+    
