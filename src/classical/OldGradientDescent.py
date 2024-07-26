ae6eea2912bff168520e07da698fd114514a3e90
Anant <anantg001@gmail.com>
2024-07-26 10:13:46 -0400

    Added testing to OldGradientDescent.py

--- src/classical/OldGradientDescent.py
+++ src/classical/OldGradientDescent.py
@@ -13,8 +13,8 @@ def gradient_descent(x0, function, r, xerr):
     count = 0
     length = len(x0)
     for ii in range(1024):
-        cost, _ = function(x0)
-        print(x0, cost, _)
+        cost = function(x0)
+        print(x0, cost)
 
         count += 1
         if cost < xerr:
@@ -23,7 +23,7 @@ def gradient_descent(x0, function, r, xerr):
         for i in range(length):
             x = x0.copy()
             x[i] += 0.01
-            cost_prime, _ = function(x)
+            cost_prime = function(x)
             count += 1
             gd[i] = (cost_prime - cost) / 0.01
         # Generate a random number r0 in range [0, 1]
@@ -36,27 +36,17 @@ def gradient_descent(x0, function, r, xerr):
     return x0
 
 
+def easom_function(x):
+    return -np.cos(x[0]) * -np.cos(x[1]) * np.exp(-((x[0] - np.pi) ** 2 + (x[1] - np.pi) ** 2))
+
+
 if __name__ == "__main__":
-    known_plaintext = generate_random_message()
-    secret_key = generate_random_key()
-
-    known_ciphertext = encrypt_sdes(known_plaintext, secret_key)
-
-    print(f'Testing with key={bits_to_string(secret_key)}')
-
-    vqe_solver = VQE_crypto(
-        known_plaintext,
-        known_ciphertext,
-        construct_graph_hamiltonian_for_ciphertext(known_ciphertext),
-        shots_per_estimate=100
-    )
-
     # Initial point for gradient descent
-    initial_point = np.random.uniform(-1, 1, 10)
+    initial_point = np.random.uniform(-1, 1, 2)
 
     optimized_point = gradient_descent(
         x0=initial_point,
-        function=lambda x: vqe_solver.run(x),
+        function=easom_function,
         r=1.82,
         xerr=-9
     )
